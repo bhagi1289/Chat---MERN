d45bf768068623f3dff128c8186d6e1f2f74eaa2
Bhagyaraju <bhagyaraju1289@gmail.com>
2024-10-20 21:19:19 +0530

    hook implementation started

---
+++ frontend/src/hooks/useSingup.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+const useSingup = () => {
+
+  const [loading, setLoading] = useState(false);
+  
+  return "";
+}
+
+export default useSingup
--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -1,44 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GenderCheckbox from './GenderCheckbox'
+import { Link } from 'react-router-dom'
 
 const Signup = () => {
+
+    const [inputs, setInputs] = useState({
+        fullName: '',
+        username: '',
+        password: '',
+        confirmPassword: '',
+        gender: ''
+    });
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        console.log(inputs);
+    }
+
+    const handleCheckBoxChange = (gender) =>{
+        setInputs({...inputs, gender})
+    }
     return (
         <div className='flex flex-col, items-center justify-center min-w-96 mx-auto'>
             <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
                 <h1 className='text-3xl font-semibold text-ce text-gray-300'>Sign Up <span className='text-blue-500' >Chat App</span></h1>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div>
                         <label className='label p-2'>
                             <span className='text-base label-text'>Full Name</span>
                         </label>
-                        <input type='text' placeholder='John Doe' className='w-full input input-bordered h-10' />
+                        <input 
+                            type='text'
+                            placeholder='John Doe' 
+                            className='w-full input input-bordered h-10' 
+                            value={inputs.fullName}
+                            onChange={(e) => setInputs({...inputs, fullName: e.target.value})}
+                        />
                     </div>
                     <div>
                         <label className='label p-2'>
                             <span className='text-base label-text'>Username</span>
                         </label>
-                        <input type='text' placeholder='John Doe' className='w-full input input-bordered h-10' />
+                        <input 
+                            type='text' 
+                            placeholder='John Doe' 
+                            className='w-full input input-bordered h-10' 
+                            value={inputs.username}
+                            onChange={(e) => setInputs({...inputs, username: e.target.value})}    
+                        />
 
                     </div>
                     <div>
                         <label className='label p-2'>
                             <span className='text-base label-text'>Password</span>
                         </label>
-                        <input type='password' placeholder='******' className='w-full input input-bordered h-10' />
+                        <input 
+                            type='password' 
+                            placeholder='******' 
+                            className='w-full input input-bordered h-10'
+                            value={inputs.password}
+                            onChange={(e) => setInputs({...inputs, password: e.target.value})}
+                             />
 
                     </div>
                     <div>
                         <label className='label p-2'>
                             <span className='text-base label-text'>Confirm Password</span>
                         </label>
-                        <input type='password' placeholder='******' className='w-full input input-bordered h-10' />
+                        <input 
+                            type='password' 
+                            placeholder='******' 
+                            className='w-full input input-bordered h-10' 
+                            onChange={(e) => setInputs({...inputs, confirmPassword: e.target.value})}  
+                        />
                     </div>
 
-                    <GenderCheckbox />
-                    <a href='#' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+                    <GenderCheckbox  onCheckBoxChange={handleCheckBoxChange} selectedGender={inputs.gender}  />
+                    
+                    <Link to='/login' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
                        Already have an account?
-                    </a>
+                    </Link>
                     <div>
                         <button className='btn btn-block btn-sm mt-2 border border-slate-700'> Sign Up</button>
                     </div>
